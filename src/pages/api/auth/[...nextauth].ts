6ddd403c7b583d98f0e0c48ad6999ce356a4f04b
Ernesto Arellano <ernestoarellano@icloud.com>
2022-07-18 19:09:23 -0700

    remove discord provider

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,11 +1,9 @@
 import NextAuth, { type NextAuthOptions } from 'next-auth'
 import CredentialsProvider from 'next-auth/providers/credentials'
-import DiscordProvider from 'next-auth/providers/discord'
 
 // Prisma adapter for NextAuth, optional and can be removed
 import { PrismaAdapter } from '@next-auth/prisma-adapter'
 import { prisma } from 'server/db/client'
-import { env } from 'server/env'
 
 export const authOptions: NextAuthOptions = {
   // Include user.id on session
@@ -20,11 +18,6 @@ export const authOptions: NextAuthOptions = {
   // Configure one or more authentication providers
   adapter: PrismaAdapter(prisma),
   providers: [
-    DiscordProvider({
-      clientId: env.DISCORD_CLIENT_ID,
-      clientSecret: env.DISCORD_CLIENT_SECRET,
-    }),
-    // ...add more providers here
     CredentialsProvider({
       name: 'Credentials',
       credentials: {
